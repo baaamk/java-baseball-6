f4735f4e7578d348610ec05874a775588a6e1845
sanghun <vitash1215@gmail.com>
2024-05-29 20:45:09 +0900

    숫자 야구 게임 구현 및 test 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import baseball.controller.BaseBallGame;
+import baseball.model.RetryNumber;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //TODO: 숫자 야구 게임 구현
+        OutputView.printGameStart();
+        do {
+            BaseBallGame baseballGame = new BaseBallGame();
+            baseballGame.start();
+        } while (askRetry());
+    }
+
+    public static boolean askRetry() {
+        RetryNumber retryNumber = new RetryNumber(InputView.setRetryNumber());
+
+        if (retryNumber.getRetryNumber().equals("1")) {
+            return true;
+        }
+
+        return false;
     }
 }
