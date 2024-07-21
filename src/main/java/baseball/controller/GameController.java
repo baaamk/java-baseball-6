476b4b3d1961d7811fc7e7d796928d764678f1eb
imjunghun <dlawndgns9@naver.com>
2024-07-22 00:16:22 +0900

    feat(GameController): 시작 문구 상수화 및 출력

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,10 @@
+package baseball.controller;
+
+public class GameController {
+
+    private final String GAME_INTRO_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public GameController() {
+        System.out.println(GAME_INTRO_MESSAGE);
+    }
+}
