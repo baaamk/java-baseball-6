f2c945a6335e16aeb55474d845524343c8c8dbc9
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-07-25 12:09:17 +0900

    feat: Game 클래스 생성

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+
+public class Game {
+    public Game() {
+    }
+
+    public void startGame(){
+        while (true) {
+            Computer computer = new Computer();
+            computer.generateNumbers();
+            System.out.println(Arrays.toString(computer.getNumbers()));
+
+            User user = new User();
+            user.startGame(computer.getNumbers());
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String restart = Console.readLine();
+
+            if (!restart.equals("1")) {
+                break;
+            }
+
+        }
+    }
+
+
+}
