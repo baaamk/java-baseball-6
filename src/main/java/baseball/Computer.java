b0c54540d47f1a43f1cd27fcb02b92ea05c3b1b4
Kyuwon Jeong <32204027@dankook.ac.kr>
2024-07-25 12:23:30 +0900

    refactor: 불필요 메서드 삭제

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -25,10 +25,6 @@ public class Computer {
         }
     }
 
-    public void printStartMessage(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
     public int[] getNumbers(){
         return this.numbers;
     }
