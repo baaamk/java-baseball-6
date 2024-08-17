92d63c69dab5f325a81b0ac6c6c5ef0935b9b2e9
minjun <ijmtylhtf@naver.com>
2024-08-17 15:39:21 +0900

    feat : 사용자의 숫자 입력 기능 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+	public static void inputNumbers() {
+		System.out.print("숫자를 입력해주세요 : ");
+		final String inputNum = Console.readLine();
+	}
 }
