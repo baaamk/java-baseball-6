87d5468911c6151c70da53980e7552cf89e995bf
minjun <ijmtylhtf@naver.com>
2024-08-17 15:52:29 +0900

    feat : 사용자 입력 숫자 리스트 변수 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,8 +1,12 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
+	private static List<Integer> inputNumList = new ArrayList<>();
 	public static void inputNumbers() {
 		System.out.print("숫자를 입력해주세요 : ");
 		final String inputNum = Console.readLine();
