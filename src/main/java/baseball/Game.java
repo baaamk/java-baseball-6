ba9a81843c8838a98277c74ce8dbfe24f615f891
minjun <ijmtylhtf@naver.com>
2024-08-17 17:11:30 +0900

    refactor : 생성자로 리스트 전달

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,18 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
 	private int strikes;
 	private int balls;
-
 	private boolean nothing;
+	private List<Integer> playerNumList = new ArrayList<>();
+	private List<Integer> computerNumList = new ArrayList<>();
 
-	public void playGame(List<Integer> playerNumList, List<Integer> computerNumList) {
+	public void playGame() {
 		for (int i = 0; i <= playerNumList.size(); i++) {
 			for(int j = 0; j <= computerNumList.size(); j++){
 
 			}
 		}
+	}
+
+	public Game(List<Integer> playerNumList, List<Integer> computerNumList) {
+		this.playerNumList = playerNumList; 
+		this.computerNumList = computerNumList;
 	}
 }
