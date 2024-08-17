97f3594eafaa44d3287dad74ae524032009fec99
minjun <ijmtylhtf@naver.com>
2024-08-17 16:37:42 +0900

    refactor : static 삭제

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -8,11 +8,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Computer {
 	private static final int startNum = 1;
 	private static final int endNum = 9;
-	private static List<Integer> pickedRandomList = new ArrayList<>();
+	private List<Integer> pickedRandomList = new ArrayList<>();
 
 	public static final Computer computer = new Computer();
 
-	public static List<Integer> pickThreeRandomNumber() {
+	public List<Integer> pickThreeRandomNumber() {
 		while (pickedRandomList.size() < 3){
 			int num = Randoms.pickNumberInRange(startNum, endNum);
 			pickedRandomList.add(num);
