6151867e61081cd5ad6bc69b493ba7c99095761c
minjun <ijmtylhtf@naver.com>
2024-08-17 15:16:05 +0900

    feat : 3개의 공을 뽑아 리스트에 저장하는 함수 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,8 +3,18 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
 	private static final int startNum = 1;
 	private static final int endNum = 9;
 	private static List<Integer> pickedRandomList = new ArrayList<>();
+
+	public static List<Integer> pickThreeRandomNumber() {
+		while (pickedRandomList.size() < 3){
+			int num = Randoms.pickNumberInRange(startNum, endNum);
+			pickedRandomList.add(num);
+		}
+		return pickedRandomList;
+	}
 }
