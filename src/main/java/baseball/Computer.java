97b5298b85e9e6a379d74b1db8a677bd7cc73f3d
minjun <ijmtylhtf@naver.com>
2024-08-17 17:46:22 +0900

    refactor : 함수명 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -12,7 +12,7 @@ public class Computer {
 
 	public static final Computer computer = new Computer();
 
-	public void pickThreeRandomNumber() {
+	public void setPickedRandomList() {
 		while (pickedRandomList.size() < 3){
 			int num = Randoms.pickNumberInRange(startNum, endNum);
 			pickedRandomList.add(num);
