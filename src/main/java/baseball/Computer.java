0f2a3b923f183bf262653e88aeae60ed510bd16e
minjun <ijmtylhtf@naver.com>
2024-08-17 15:14:40 +0900

    feat : Computer에서 뽑은 3개의 번호 리스트 변수 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,6 +1,10 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
 	private static final int startNum = 1;
-	private static final int endNum = 9; 
+	private static final int endNum = 9;
+	private static List<Integer> pickedRandomList = new ArrayList<>();
 }
