0b2ffa51c17b454356fe52442a89440d5d6f8f41
minjun <ijmtylhtf@naver.com>
2024-08-17 15:48:15 +0900

    feat : 입력값 검증 수행

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,6 +6,7 @@ public class Player {
 	public static void inputNumbers() {
 		System.out.print("숫자를 입력해주세요 : ");
 		final String inputNum = Console.readLine();
+		validateLength(inputNum);
 	}
 
 	private static void validateLength(final String inputNum){
