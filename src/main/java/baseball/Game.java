455c856570cdfb2d3e4566abae7674c85db6eb3d
minjun <ijmtylhtf@naver.com>
2024-08-17 17:05:53 +0900

    feat : 함수 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,8 +1,18 @@
 package baseball;
 
+import java.util.List;
+
 public class Game {
 	private int strikes;
 	private int balls;
 
-	private boolean nothing; 
+	private boolean nothing;
+
+	public void playGame(List<Integer> playerNumList, List<Integer> computerNumList) {
+		for (int i = 0; i <= playerNumList.size(); i++) {
+			for(int j = 0; j <= computerNumList.size(); j++){
+
+			}
+		}
+	}
 }
