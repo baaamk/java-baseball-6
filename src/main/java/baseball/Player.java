dd656f4cc17113b4bb73fe29e1faedf7791787f0
minjun <ijmtylhtf@naver.com>
2024-08-17 15:54:02 +0900

    refactor : 반환값 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,10 +7,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
 	private static List<Integer> inputNumList = new ArrayList<>();
-	public static void inputNumbers() {
+	public static String inputNumbers() {
 		System.out.print("숫자를 입력해주세요 : ");
 		final String inputNum = Console.readLine();
 		validateLength(inputNum);
+		return inputNum;
 	}
 
 	private static void validateLength(final String inputNum){
