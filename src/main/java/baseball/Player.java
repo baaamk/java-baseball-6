4d4d792c4ebba78fa6774aa24015354869aa9697
minjun <ijmtylhtf@naver.com>
2024-08-17 15:47:38 +0900

    feat : 입력값 길이 검증 함수 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,4 +7,9 @@ public class Player {
 		System.out.print("숫자를 입력해주세요 : ");
 		final String inputNum = Console.readLine();
 	}
+
+	private static void validateLength(final String inputNum){
+		if(inputNum.length() != 3)
+			throw new IllegalArgumentException();
+	}
 }
