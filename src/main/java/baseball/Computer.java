3f8e61ae4cb32bdce323ec095934d6359cc2942a
minjun <ijmtylhtf@naver.com>
2024-08-17 15:21:27 +0900

    feat : Computer 싱글톤 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -10,11 +10,16 @@ public class Computer {
 	private static final int endNum = 9;
 	private static List<Integer> pickedRandomList = new ArrayList<>();
 
+	public static final Computer computer = new Computer();
+
 	public static List<Integer> pickThreeRandomNumber() {
 		while (pickedRandomList.size() < 3){
 			int num = Randoms.pickNumberInRange(startNum, endNum);
 			pickedRandomList.add(num);
 		}
 		return pickedRandomList;
+	}
+
+	private Computer() {
 	}
 }
