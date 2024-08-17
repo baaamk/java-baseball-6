492158e2bf262b858c160c1baf2010f631c608f3
minjun <ijmtylhtf@naver.com>
2024-08-17 15:55:44 +0900

    refactor : static 삭제

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,7 +6,7 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
-	private static List<Integer> inputNumList = new ArrayList<>();
+	private List<Integer> inputNumList = new ArrayList<>();
 	public static String inputNumbers() {
 		System.out.print("숫자를 입력해주세요 : ");
 		final String inputNum = Console.readLine();
