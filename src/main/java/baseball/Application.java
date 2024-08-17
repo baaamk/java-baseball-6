006bea441d9ec63928fed77d240b83803dc296c6
minjun <ijmtylhtf@naver.com>
2024-08-17 17:45:19 +0900

    feat : 사용자 숫자 리스트 반환 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> getPlayerNumList() {
+        Player player = Player.player;
+        player.setInputNumList();
+        player.changeToList();
+        return player.getInputNumList();
     }
 }
