1564d5b4b2baac71b606681cb1264718a2734641
sunghwan <tjdghks130907@gmail.com>
2024-11-20 22:47:28 +0900

    feat: 1~9 까지 서로 다른 임의의 수 3개를 생성하는 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,11 +1,35 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     private List<Integer> numbers;
 
+    public Computer() {
+        this.numbers = new ArrayList<>();
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public void createNumber() {
+        if (numbers.size() == 3) {
+            throw new IllegalCallerException("한 번 생성된 랜덤 3자리 숫자를 재생성 할 수 없습니다.");
+        }
+
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
+
+    public void clearNumber() {
+        numbers.clear();
     }
 }
