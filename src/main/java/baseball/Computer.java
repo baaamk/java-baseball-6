62f69c467881c420c8930a4d32674814ff224b0b
sunghwan <tjdghks130907@gmail.com>
2024-11-26 23:13:52 +0900

    refactor: 컴퓨터 번호 생성 시 기존 번호 초기화 후 생성하도록 변경, 초기화 메서드는 private로 접근제어자 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -17,9 +17,7 @@ public class Computer {
     }
 
     public void createNumber() {
-        if (numbers.size() == 3) {
-            throw new IllegalCallerException("한 번 생성된 랜덤 3자리 숫자를 재생성 할 수 없습니다.");
-        }
+        clearNumber();
 
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -29,7 +27,7 @@ public class Computer {
         }
     }
 
-    public void clearNumber() {
+    private void clearNumber() {
         numbers.clear();
     }
 }
