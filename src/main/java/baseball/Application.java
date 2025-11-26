6a0353f88cbeddc262cd383beb8ea58854a7d021
seop <useop0821@gmail.com>
2025-11-27 02:16:23 +0900

    feat: 메인 애플리케이션 구현 및 컨트롤러 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import baseball.controller.BaseBallController;
+import baseball.model.GameProgress;
+import baseball.model.RandomNumberGenerator;
+import baseball.view.ConsoleInput;
+import baseball.view.ConsoleOutput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        ConsoleInput consoleInput = new ConsoleInput();
+        ConsoleOutput consoleOutput = new ConsoleOutput();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        GameProgress gameProgress = new GameProgress();
+
+        BaseBallController baseBallController = new BaseBallController(
+                consoleInput,
+                consoleOutput,
+                randomNumberGenerator,
+                gameProgress
+        );
+
+        baseBallController.run();
     }
 }
