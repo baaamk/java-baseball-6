57b755bedc0f88fb88e2ce4b5b66d9a201a5bbd8
baaamk <brandon55321@gmail.com>
2025-12-31 22:39:06 +0900

    refactor: 메서드명 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,11 +3,11 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public String inputNumberPrint() {
+    public String inputNumber() {
         return Console.readLine();
     }
 
-    public String retryInputPrint() {
+    public String inputRetry() {
         return Console.readLine();
     }
 }
