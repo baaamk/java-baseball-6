d707d911016e461efb73eff0f375d1a4740aa6e8
baaamk <brandon55321@gmail.com>
2025-12-31 22:41:19 +0900

    feat: 비교 결과반환 서비스 레이어 구현

---
+++ src/main/java/baseball/service/Service.java
@@ -0,0 +1,14 @@
+package baseball.service;
+
+import baseball.model.domain.BaseballNumbers;
+import baseball.model.domain.Result;
+
+import java.util.List;
+
+public class Service {
+    public Result showResult(BaseballNumbers baseballNumbers,List<Integer> parsedInputNumbers) {
+        BaseballNumbers usernumbers = BaseballNumbers.of(parsedInputNumbers);
+        return baseballNumbers.compareNumber(usernumbers);
+    }
+}
+
