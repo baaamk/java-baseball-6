873c89e4fa6c1318a09e71681ca817d04fd7cdc9
baaamk <brandon55321@gmail.com>
2025-12-31 18:17:36 +0900

    feat: 원시 클래스와 래핑 클래스 구현

---
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+public class BaseballNumber {
+    private final int baseballNumber;
+
+    private BaseballNumber(int baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+
+    public static BaseballNumber from(int baseballNumber) {
+        return new BaseballNumber(baseballNumber);
+    }
+}
