79d995d25743e9fb40c2b9c9ab3b546ee0dbb373
baaamk <brandon55321@gmail.com>
2025-12-31 19:13:54 +0900

    feat: BaseballNumber equals 재정의

--- src/main/java/baseball/model/domain/vo/BaseballNumber.java
+++ src/main/java/baseball/model/domain/vo/BaseballNumber.java
@@ -1,5 +1,7 @@
 package baseball.model.domain.vo;
 
+import java.util.Objects;
+
 public class BaseballNumber {
     private final int baseballNumber;
 
@@ -9,5 +11,17 @@ public class BaseballNumber {
 
     public static BaseballNumber from(int baseballNumber) {
         return new BaseballNumber(baseballNumber);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseballNumber that = (BaseballNumber) o;
+        return baseballNumber == that.baseballNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(baseballNumber);
     }
 }
