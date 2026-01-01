8d17a253cc3cff16f3ca48f8eda589a883b3db4c
baaamk <brandon55321@gmail.com>
2026-01-01 20:03:13 +0900

    feat: 에러 기능 추가 구현

---
+++ src/main/java/baseball/utils/ErrorMessage.java
@@ -0,0 +1,16 @@
+package baseball.utils;
+
+public enum ErrorMessage {
+    INVALID_DUPLICATE_NUMBER("1~9 사이의 중복 없는 3자리 숫자를 입력하세요."),
+    INVALID_RETRY_NUMBER("1 또는 2의 숫자만 입력해 주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
