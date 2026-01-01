c5782bc5c2d1393d9ff26723f28c9c66feac27fa
baaamk <brandon55321@gmail.com>
2026-01-01 20:21:41 +0900

    refactor: 코드 컨벤션 적용 수정

--- src/main/java/baseball/service/Service.java
+++ src/main/java/baseball/service/Service.java
@@ -6,7 +6,7 @@ import baseball.model.domain.Result;
 import java.util.List;
 
 public class Service {
-    public Result showResult(BaseballNumbers baseballNumbers,List<Integer> parsedInputNumbers) {
+    public Result showResult(BaseballNumbers baseballNumbers, List<Integer> parsedInputNumbers) {
         BaseballNumbers usernumbers = BaseballNumbers.of(parsedInputNumbers);
         return baseballNumbers.compareNumber(usernumbers);
     }
