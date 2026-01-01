a252044c3b8bd6b546e008663aecf97c7cf56a29
baaamk <brandon55321@gmail.com>
2026-01-01 20:04:08 +0900

    feat: 파싱 단계에 검증 기능 구현

--- src/main/java/baseball/view/parser/StringParser.java
+++ src/main/java/baseball/view/parser/StringParser.java
@@ -1,20 +1,41 @@
 package baseball.view.parser;
 
+import baseball.utils.ErrorMessage;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class StringParser {
 
-    private StringParser(){
+    private static final String NUMBER_REGEX = "^(?!.*(.).*\\1)[1-9]{3}$";
+    private static final String RETRY_NUMBER_REGEX = "^[12]$";
+
+    private StringParser() {
     }
 
-    public static List<Integer> parseToList(String inputNumbers){
+    public static List<Integer> parseToList(String inputNumbers) {
+        validateInputNumber(inputNumbers);
+
         return Arrays.stream(inputNumbers.split(""))
                 .map(Integer::parseInt)
                 .toList();
     }
 
     public static int parseToInt(String inputRetry) {
+        validateRetryNumber(inputRetry);
         return Integer.parseInt(inputRetry);
     }
+
+    private static void validateInputNumber(String inputNumbers) {
+        if (!inputNumbers.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DUPLICATE_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateRetryNumber(String inputRetry) {
+        if (!inputRetry.matches(RETRY_NUMBER_REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RETRY_NUMBER.getMessage());
+        }
+    }
+
 }
